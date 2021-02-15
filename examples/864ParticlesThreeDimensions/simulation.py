b55ca677ea042402588074a65f0592679f698945
evenmn <evenmn@fys.uio.no>
2021-02-15 23:12:19 +0100

    Updated example

--- examples/864ParticlesThreeDimensions/simulation.py
+++ examples/864ParticlesThreeDimensions/simulation.py
@@ -7,7 +7,7 @@ Integrator: Velocity-Verlet
 """
 
 from mdsolver import MDSolver
-from mdsolver.initpositions import FCC
+from mdsolver.initpositions import FCC, Restart
 from mdsolver.initvelocities import Temperature
 from mdsolver.boundaryconditions import Periodic
 
@@ -15,8 +15,13 @@ solver = MDSolver(positions=FCC(cells=6, lenbulk=10),
                   velocities=Temperature(T=1.5),
                   boundaries=Periodic(lenbox=12),
                   dt=0.01)
+
+# equilibration run
+solver.thermo(10, "864N_3D.log", "step", "time")
+solver.run(steps=100)
+
+# production run
 solver.dump(10, "864N_3D.xyz", "x", "y", "z")
 solver.thermo(10, "864N_3D.log", "step", "time", "poteng", "kineng")
-solver.run(steps=100)
 solver.run(steps=100)
 solver.snapshot("snapshot.xyz")
