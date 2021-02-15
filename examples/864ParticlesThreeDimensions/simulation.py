c152ad8f45ef0e91318132fb1f621fcc53c44027
evenmn <evenmn@fys.uio.no>
2021-02-15 18:02:59 +0100

    Added modified example

--- examples/864ParticlesThreeDimensions/simulation.py
+++ examples/864ParticlesThreeDimensions/simulation.py
@@ -14,15 +14,14 @@ from mdsolver.potential import LennardJones
 from mdsolver.integrator import VelocityVerlet
 from mdsolver.initpositions import FCC
 from mdsolver.initvelocities import Temperature
-from mdsolver.boundaryconditions import Periodic
+from mdsolver.boundaryconditions import Periodic, Reflective
 
-solver = MDSolver(positions=FCC(cells=6, lenbulk=10), 
-                  velocities=Temperature(T=300),
+solver = MDSolver(positions=FCC(cells=6, lenbulk=10),
+                  velocities=Temperature(T=1.5),
                   boundaries=Periodic(lenbox=12),
-                  T=5, 
+                  T=5,
                   dt=0.01)
-solver(potential=LennardJones(solver, cutoff=3), 
-       integrator=VelocityVerlet(solver),
-       dumpfile="864N_3D.data")
-solver.plot_energy()
-solver.plot_temperature()
+solver.dump(10, "864N_3D.xyz", "x", "y", "z")
+solver.thermo(10, "864N_3D.log", "step", "time", "poteng", "kineng")
+solver.run(potential=LennardJones(solver, cutoff=3),
+       integrator=VelocityVerlet(solver))
